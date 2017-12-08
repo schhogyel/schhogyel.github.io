177818556e65eb8d8e7a6405d08a1387b13ab2bd
Sonam Chhogyel <schyles@gmail.com>
2017-12-08 16:32:02 +1000

    Add different hightlight theme

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,7 @@ import Link from "gatsby-link";
 import Helmet from "react-helmet";
 import styled from "styled-components";
 import Header from "./Header";
-require("prismjs/themes/prism-solarizedlight.css");
+require("./prism-tommorow.css");
 
 import "./index.css";
 import { NavDrawer } from "../components/NavDrawer";
